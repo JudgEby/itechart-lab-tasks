e398c21cd2ae33d21e6ddd6d7976da070d38a49a
Yan Dobrovolskiy <bloodwoodwood@gmail.com>
2021-10-14 10:34:47 +0300

    rewrote to classes

--- popups/scriptES6.js
+++ popups/scriptES6.js
@@ -5,9 +5,11 @@ var successButton = document.getElementById("success");
 var infoButton = document.getElementById("info");
 var modalButton = document.getElementById("modal");
 
-function PopUp(container) {
-  this.container = container || document.getElementsByTagName("body")[0];
-  this._greateNewElement = function (tagName, id) {
+class PopUp {
+  constructor(container) {
+    this.container = container || document.getElementsByTagName("body")[0];
+  }
+  _createNewElement(tagName, id) {
     var newElement = document.createElement(tagName);
     if (id) {
       newElement.id = id;
@@ -15,104 +17,113 @@ function PopUp(container) {
 
     newElement.className = [].slice.call(arguments).slice(2).join(" ");
     return newElement;
-  };
+  }
 }
 
-function ToastNotifications(container, popUpType) {
-  PopUp.apply(this, arguments);
-  this.popUpType = popUpType;
-}
-
-ToastNotifications.prototype._buildPopUp = function (popUpType, message) {
-  var validPopUpType = popUpType;
-  if (
-    popUpType !== "error" &&
-    popUpType !== "warning" &&
-    popUpType !== "success"
-  ) {
-    validPopUpType = "info";
+class ToastNotifications extends PopUp {
+  constructor(container, popUpType) {
+    super(container);
+    this.popUpType = popUpType;
   }
 
-  var popUpMessage = message || "Something went wrong";
+  _buildPopUp(popUpType, message) {
+    var validPopUpType = popUpType;
+    if (
+      popUpType !== "error" &&
+      popUpType !== "warning" &&
+      popUpType !== "success"
+    ) {
+      validPopUpType = "info";
+    }
 
-  var popupWrapper = this._greateNewElement(
-    "div",
-    undefined,
-    "popup-wrapper",
-    validPopUpType
-  );
+    var popUpMessage = message || "Something went wrong";
 
-  var icon = this._greateNewElement("div", undefined, "icon", validPopUpType);
-  popupWrapper.appendChild(icon);
+    var popupWrapper = this._createNewElement(
+      "div",
+      undefined,
+      "popup-wrapper",
+      validPopUpType
+    );
 
-  var popup = this._greateNewElement("span");
-  popup.innerText = popUpMessage;
-  popupWrapper.appendChild(popup);
+    var icon = this._createNewElement("div", undefined, "icon", validPopUpType);
+    popupWrapper.appendChild(icon);
 
-  var closeButton = this._greateNewElement("div", undefined, "cl-btn");
-  closeButton.addEventListener("click", function () {
-    popupWrapper.remove();
-  });
-  popupWrapper.appendChild(closeButton);
+    var popup = this._createNewElement("span");
+    popup.innerText = popUpMessage;
+    popupWrapper.appendChild(popup);
 
-  return popupWrapper;
-};
+    var closeButton = this._createNewElement("div", undefined, "cl-btn");
+    closeButton.addEventListener("click", function () {
+      popupWrapper.remove();
+    });
+    popupWrapper.appendChild(closeButton);
 
-ToastNotifications.prototype._runTimer = function (popUpNode, timeToShow) {
-  var timeout = setTimeout(function () {
-    popUpNode.remove();
-    clearTimeout(timeout);
-  }, timeToShow);
-};
-
-ToastNotifications.prototype.show = function (message, time) {
-  var timeToShow = time || 5000;
-
-  var popupWrapper = this._buildPopUp(this.popUpType, message);
-
-  var popupBlock = document.getElementById("popup-block");
-
-  if (popupBlock) {
-    popupBlock.appendChild(popupWrapper);
-  } else {
-    popupBlock = this._greateNewElement("div", "popup-block", "popup-block");
-    popupBlock.id = "popup-block";
-    popupBlock.className = "popup-block";
-    popupBlock.appendChild(popupWrapper);
-    this.container.appendChild(popupBlock);
+    return popupWrapper;
   }
 
-  this._runTimer(popupWrapper, timeToShow);
-};
+  _runTimer(popUpNode, timeToShow) {
+    var timeout = setTimeout(function () {
+      popUpNode.remove();
+      clearTimeout(timeout);
+    }, timeToShow);
+  }
 
-function ModalWindow(container) {
-  PopUp.apply(this, arguments);
-  this.content = null;
+  show(message, time) {
+    var timeToShow = time || 5000;
+
+    var popupWrapper = this._buildPopUp(this.popUpType, message);
+
+    var popupBlock = document.getElementById("popup-block");
+
+    if (popupBlock) {
+      popupBlock.appendChild(popupWrapper);
+    } else {
+      popupBlock = this._createNewElement("div", "popup-block", "popup-block");
+      popupBlock.id = "popup-block";
+      popupBlock.className = "popup-block";
+      popupBlock.appendChild(popupWrapper);
+      this.container.appendChild(popupBlock);
+    }
+
+    this._runTimer(popupWrapper, timeToShow);
+  }
 }
-ModalWindow.prototype.show = function (content) {
-  this.content =
-    content ||
-    (function () {
-      var defaultContent = document.createElement("div");
-      defaultContent.innerText = "Modal";
-      return defaultContent;
-    })();
 
-  var modalContainer = document.createElement("div");
-  modalContainer.className = "modal-container";
-  var modalBackground = document.createElement("div");
-  modalBackground.className = "modal-background";
-  modalBackground.addEventListener("click", function () {
-    modalContainer.remove();
-  });
-  var modalMain = document.createElement("div");
-  modalMain.className = "modal-main";
+class ModalWindow extends PopUp {
+  constructor(container) {
+    super(container);
+    this.content = null;
+  }
+  show(content) {
+    this.content =
+      content ||
+      (function () {
+        var defaultContent = document.createElement("div");
+        defaultContent.innerText = "Modal";
+        return defaultContent;
+      })();
 
-  modalMain.appendChild(this.content);
-  modalContainer.appendChild(modalMain);
-  modalContainer.appendChild(modalBackground);
-  this.container.appendChild(modalContainer);
-};
+    var modalContainer = this._createNewElement(
+      "div",
+      undefined,
+      "modal-container"
+    );
+    var modalBackground = this._createNewElement(
+      "div",
+      undefined,
+      "modal-background"
+    );
+    modalBackground.addEventListener("click", function () {
+      modalContainer.remove();
+    });
+    var modalMain = this._createNewElement("div", undefined, "modal-main");
+
+    modalMain.appendChild(this.content);
+    modalContainer.appendChild(modalMain);
+    modalContainer.appendChild(modalBackground);
+    this.container.appendChild(modalContainer);
+  }
+}
 
 //добавляем события на кнопки
 modalButton.addEventListener("click", function () {
